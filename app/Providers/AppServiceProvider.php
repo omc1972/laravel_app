e35ece9063b0a3b3c8ca2ae46204fcab27c535db
oscar.mendoza <oscar.mendoza@hacce.com>
2023-05-05 12:58:22 +0200

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Providers;
+
+use App\Lib\DbSessionStorage;
+use App\Lib\Handlers\AppUninstalled;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Shopify\Context;
+use Shopify\ApiVersion;
+use Shopify\Webhooks\Registry;
+use Shopify\Webhooks\Topics;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     * @throws \Shopify\Exception\MissingArgumentException
+     */
+    public function boot()
+    {
+        Context::initialize(
+            Config::get('shopify.api_key'),
+            Config::get('shopify.api_secret'),
+            Config::get('shopify.scopes'),
+            str_replace('https://', '', Config::get('shopify.host')),
+            new DbSessionStorage(),
+            ApiVersion::LATEST
+        );
+
+        URL::forceScheme('https');
+
+        Registry::addHandler(Topics::APP_UNINSTALLED, new AppUninstalled());
+    }
+}
