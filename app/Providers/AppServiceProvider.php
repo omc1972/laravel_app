5735e9e95738a29a0892241858a5094456a514e3
oscar.mendoza <oscar.mendoza@hacce.com>
2023-05-08 10:40:38 +0200

    context.phpo update with credentials

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,15 +32,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Context::initialize(
+       /* Context::initialize(
             Config::get('shopify.api_key'),
             Config::get('shopify.api_secret'),
             Config::get('shopify.scopes'),
             str_replace('https://', '', Config::get('shopify.host')),
             new DbSessionStorage(),
             ApiVersion::LATEST
+        ); */
+        Context::initialize(
+            "0ed7632b923496aa0c99a70cb4cd20a2",
+            "4904d7a085a211936337fe27808e6835",
+            "write_products,write_draft_orders,write_customers",
+            str_replace('https://', '', "https://laravel-test-app.herokuapp.com/"),
+            new DbSessionStorage(),
+            ApiVersion::LATEST
         );
-
         URL::forceScheme('https');
 
         Registry::addHandler(Topics::APP_UNINSTALLED, new AppUninstalled());
